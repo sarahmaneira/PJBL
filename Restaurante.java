a45ba3a39ef508bc53be8e0e905fb5cc24f41d67
EnzoRicardo <enzoricardodeoliveira@gmail.com>
2024-10-23 22:39:31 -0300

    aula 23 do 10

--- Restaurante.java
+++ Restaurante.java
@@ -1,6 +1,43 @@
-public class Restaurante {
-    public static void main(String[] args) {
-        System.out.println("aaaaaaaaaaa");
-        System.out.println("teste");
+
+class Prato{
+    String nome;
+    double valor;
+    String descricao;
+
+    public Prato(String nome, double valor, String descricao){
+        this.nome = nome;
+        this.valor = valor;
+        this.descricao = descricao;
+    }
+}
+
+class Cliente{
+    String nome;
+    String numeroTelefone;
+    String pedidos = "";
+
+    public void fazerPedido(String novoPedido){
+        if (pedidos.isEmpty()){
+            pedidos += ", ";
+        }
+
+        pedidos += novoPedido;
+    }
+
+    public String consultarPedido() {
+        if (pedidos.isEmpty()) {
+            return "Nenhum pedido realizado.";
+        }
+        return pedidos;
+    }
+}
+
+class Pedido{
+    int numero;
+    String descricao;
+    int valor;
+
+    public void calcularTotal(){
+
     }
 }
