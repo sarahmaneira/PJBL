0342149aa34bc8e7ac58d24eb64a9d6e4cdb7538
maneira <sarahmaneira17@gmail.com>
2024-10-30 21:26:54 -0300

    classe main - lendo um arquivo de texto

--- PJBL.java
+++ PJBL.java
@@ -1,4 +1,16 @@
 import java.util.ArrayList;
+import java.io.*;
+
+class PJBL {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileIn = new FileInputStream(pratos.ser);
+        ObjectInputStream objIn = new ObjectInputStream(fileIn);
+        Prato p = (Prato) objIn.readObject();
+
+        objIn.close();
+        fileIn.close();
+    }
+}
 
 class Restaurante{
     private String nome;
