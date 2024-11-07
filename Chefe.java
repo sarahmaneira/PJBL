b23b39c0859c7bd79aeec93c132485612324c97f
maneira <sarahmaneira17@gmail.com>
2024-11-06 21:17:47 -0300

    tentando adicionar pratos em cada chefe

--- Chefe.java
+++ Chefe.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
+
 class Chefe extends Funcionario {
+    private String identificador;
     private double valorFixo;
     private int horasTrabalhadas;
+    private ArrayList<Prato> prato = new ArrayList<>();
 
-    public Chefe(String nome, String cargo, int idade, double salario, String genero, double valorFixo, int horasTrabalhadas) {
+    public Chefe(String nome, String cargo, int idade, double salario, String genero, double valorFixo, int horasTrabalhadas, String identificador) {
         super(nome, cargo, idade, salario, genero);
         this.valorFixo = valorFixo;
         this.horasTrabalhadas = horasTrabalhadas;
+        this.identificador = identificador;
+    }
+
+    public void adicionarPrato(Prato p){
+        prato.add(p);
     }
 
    public double getValorFixo(){
