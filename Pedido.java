568c09899470e201502dee740b28de2866808b42
maneira <sarahmaneira17@gmail.com>
2024-11-10 00:47:13 -0300

    construtor pedido

--- Pedido.java
+++ Pedido.java
@@ -3,6 +3,11 @@ class Pedido {
     String descricao;
     int valor;
 
+    public Pedido(int numero, String descricao, int valor){
+        this.numero = numero;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
     public void calcularTotal(){
     }
 
